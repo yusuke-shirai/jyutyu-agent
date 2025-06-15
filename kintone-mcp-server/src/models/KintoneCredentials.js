11ebc5fcac434b22698275332e9472f55c0bd91e
yusuke-shirai <Shirai.Yusuke@gmail.com>
2025-06-15 23:34:59 +0900

    Delete kintone-mcp-server directory

--- kintone-mcp-server/src/models/KintoneCredentials.js
+++
@@ -1,9 +0,0 @@
-// src/models/KintoneCredentials.js
-export class KintoneCredentials {
-    constructor(domain, username, password) {
-        this.domain = domain;
-        this.username = username;
-        this.password = password;
-        this.auth = Buffer.from(`${username}:${password}`).toString('base64');
-    }
-}
--- kintone-mcp-server/src/models/KintoneRecord.js
+++
@@ -1,8 +0,0 @@
-// src/models/KintoneRecord.js
-export class KintoneRecord {
-    constructor(appId, recordId, fields) {
-        this.appId = appId;
-        this.recordId = recordId;
-        this.fields = fields;
-    }
-}
